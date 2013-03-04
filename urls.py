04a2b6dfb3e85f9d41fbbb0299e092700e47e57e
arming <arming@Arming.(none)>
2013-03-04 13:45:00 +0800

    add group handler

--- urls.py
+++ urls.py
@@ -4,10 +4,12 @@ from  handlers.home import HomeHandler
 from  handlers.login import LoginHandler
 from  handlers.validate import ValidateHandler
 from  handlers.signup import SignupHandler
+from handlers.group import GroupHandler
 url_patterns = [
         (r"/", HomeHandler),
         (r"/login",LoginHandler),
         (r"/validate",ValidateHandler),
-        (r"/signup",SignupHandler)
+        (r"/signup",SignupHandler),
+        (r"/group/([0-9]+)", GroupHandler),
         ]
 
