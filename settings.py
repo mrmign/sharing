45254dc22213e9c67c02654380cd2bf15b45db9f
arming <arming@Arming.(none)>
2013-03-08 18:08:22 +0800

    add auth modul

--- settings.py
+++ settings.py
@@ -8,5 +8,7 @@ settings = dict(
         static_path = os.path.join(os.path.dirname(__file__), "static"),
         template_path = os.path.join(os.path.dirname(__file__), "templates"),
         ui_modules = {"HomeList": HomeListModule},
+        cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+        login_url="/login",
         debug=True,
         )
