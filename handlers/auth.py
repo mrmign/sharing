d4fa4afe76292e224cfe6d761a27da1a8e1c7079
arming <arming@Arming.(none)>
2013-03-08 18:06:23 +0800

    move login,signup to auth

---
+++ handlers/auth.py
@@ -0,0 +1,56 @@
+#coding=utf-8
+
+import tornado.web
+from base import BaseHandler
+
+from models.database import User
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        if self.current_user:
+            print self.current_user.id, self.current_user.username
+        self.render("login.html",login_msg=" ")
+
+    def post(self):
+        username=self.get_argument('username')
+        password=self.get_argument('password')
+        user = self.db.find(User,User.username==username, User.password==password).one()
+        
+        if user:
+            self.set_secure_cookie("share_user", str(user.id))
+            self.render("me.html",user=user)
+            
+        else:
+            self.render(
+                "login.html" ,login_msg="your username or password is wrong,please print again"                
+                )
+
+
+class SignupHandler(BaseHandler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        username = self.get_argument('username')
+        email = self.get_argument("email")
+        password = self.get_argument("password")
+        # print username, email, password
+        user1 = self.db.find(User,User.username==username).one()
+        user2 = self.db.find(User,User.email==email).one()
+        # print user1e
+        if not user1 and not user2:
+            user = User()
+            user.username = username
+            user.email = email
+            user.password = password
+            self.db.add(user)
+            self.db.commit()
+            self.render("login.html", login_msg="signup successfully,please login!")
+        else:
+            self.render("signup.html")
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("share_user")
+        self.redirect("/")
