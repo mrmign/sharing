bf5c853130441fede77965a248f43766555c7d74
sa <sa@sa-Rev-1-0.(none)>
2013-03-09 08:54:43 -0500

    mygroups

--- handlers/group.py
+++ handlers/group.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 
+from storm.locals import AutoReload
 from base import BaseHandler
 from models.database import LinkGroup
 
@@ -9,3 +10,11 @@ class GroupHandler(BaseHandler):
 		# print groupid
 		# print group.group_name
 		self.render("group.html",group=group)
+
+class MeGroupHandler(BaseHandler):
+	def get(self, groupid):
+		group = self.db.get(LinkGroup, int(groupid))
+		print group.links.count()
+		print group.group_name
+		# group = AutoReload
+		self.render("megroup.html",group=group,user=self.current_user)
--- urls.py
+++ urls.py
@@ -1,17 +1,18 @@
 #coding:utf-8
 
 from  handlers.home import HomeHandler
-from  handlers.login import LoginHandler
+# from  handlers.login import LoginHandler
 # from  handlers.validate import ValidateHandler
 # from  handlers.signup import SignupHandler
-from handlers.auth import (LoginHandler,SignupHandler,LoginHandler)
-from handlers.group import GroupHandler
+from handlers.auth import (LoginHandler,SignupHandler,LogoutHandler)
+from handlers.group import (GroupHandler,MeGroupHandler)
 url_patterns = [
         (r"/", HomeHandler),
         (r"/login",LoginHandler),
-        (r"/logout",LoginHandler),
+        (r"/logout",LogoutHandler),
         (r"/signup",SignupHandler),
         (r"/group/([0-9]+)", GroupHandler),
+        (r"/me/group/([0-9]+)",MeGroupHandler),
         ]
 
  
