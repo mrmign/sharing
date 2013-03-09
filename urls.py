94ca0d1b7d8662669f0f4ab1b91c92e270cb0e11
yy <yy@yy-Lenovo-V370.(none)>
2013-03-09 21:28:29 +0800

    add feed (not finish) and all links

--- urls.py
+++ urls.py
@@ -1,17 +1,20 @@
 #coding:utf-8
 
 from  handlers.home import HomeHandler
-from  handlers.login import LoginHandler
+# from  handlers.login import LoginHandler
 # from  handlers.validate import ValidateHandler
 # from  handlers.signup import SignupHandler
-from handlers.auth import (LoginHandler,SignupHandler,LoginHandler)
+from handlers.auth import (LoginHandler,SignupHandler,LogoutHandler)
+from handlers.me import (FeedHandler,MyLinksHandler)
 from handlers.group import GroupHandler
 url_patterns = [
         (r"/", HomeHandler),
         (r"/login",LoginHandler),
-        (r"/logout",LoginHandler),
+        (r"/logout",LogoutHandler),
         (r"/signup",SignupHandler),
         (r"/group/([0-9]+)", GroupHandler),
+        (r"/feed", FeedHandler),
+        (r"/mylinks", MyLinksHandler),
         ]
 
  
