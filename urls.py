3d56547c18b5509cbef08e92a25209ce1f395682
yy <yy@yy-Lenovo-V370.(none)>
2013-03-10 21:31:57 +0800

    add some url

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@
 
 from  handlers.home import HomeHandler
 
+from  handlers.following import FollowHandler
 from handlers.auth import (LoginHandler,SignupHandler,LogoutHandler)
 from handlers.me import (FeedHandler,
                          MyLinksHandler,
@@ -19,12 +20,14 @@ url_patterns = [
         (r"/logout",LogoutHandler),
         (r"/signup",SignupHandler),
         (r"/group/([0-9]+)", GroupHandler),
-        (r"/feed", FeedHandler),
-        (r"/mylinks", MyLinksHandler),
+        (r"/me/feed", FeedHandler),
+        (r"/me/mylinks", MyLinksHandler),
         (r"/me/group/([0-9]+)",MeGroupHandler),
-        (r"/staff_picks",StaffPicksHandler),
-        (r"/popular_groups",PopularGroupsHandler),
-        (r"/recent_links",RecentLinksHandler),
+        (r"/me/staff_picks",StaffPicksHandler),
+        (r"/me/popular_groups",PopularGroupsHandler),
+        (r"/me/recent_links",RecentLinksHandler),
+        (r"/follow/user/([0-9]+)",FollowHandler)
+        
         ]
 
  
