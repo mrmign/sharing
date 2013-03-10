80640fd39c4eb5819ff78174eadd872d5500f584
yy <yy@yy-Lenovo-V370.(none)>
2013-03-10 18:57:55 +0800

    me_nocontent,staff_picks,popular_groups,recent_links,urls.py ,me.py,me1.css

--- urls.py
+++ urls.py
@@ -3,7 +3,13 @@
 from  handlers.home import HomeHandler
 
 from handlers.auth import (LoginHandler,SignupHandler,LogoutHandler)
-from handlers.me import (FeedHandler,MyLinksHandler,MeGroupHandler)
+from handlers.me import (FeedHandler,
+                         MyLinksHandler,
+                         MeGroupHandler,
+                         StaffPicksHandler,
+                         PopularGroupsHandler,
+                         RecentLinksHandler
+                         )
 
 from handlers.group import GroupHandler
 
@@ -16,6 +22,9 @@ url_patterns = [
         (r"/feed", FeedHandler),
         (r"/mylinks", MyLinksHandler),
         (r"/me/group/([0-9]+)",MeGroupHandler),
+        (r"/staff_picks",StaffPicksHandler),
+        (r"/popular_groups",PopularGroupsHandler),
+        (r"/recent_links",RecentLinksHandler),
         ]
 
  
