952e295bd29475ebd892499458669720b482de43
yy <yy@yy-Lenovo-V370.(none)>
2013-03-11 13:47:14 +0800

    module: following and follower

--- urls.py
+++ urls.py
@@ -2,31 +2,43 @@
 
 from  handlers.home import HomeHandler
 
-from  handlers.following import FollowHandler
+from  handlers.following import (FollowUserHandler,FollowGroupHandler)
 from handlers.auth import (LoginHandler,SignupHandler,LogoutHandler)
 from handlers.me import (FeedHandler,
                          MyLinksHandler,
                          MeGroupHandler,
                          StaffPicksHandler,
                          PopularGroupsHandler,
-                         RecentLinksHandler
+                         RecentLinksHandler,
+                         FollowingHandler,
+                         FollowerHandler
                          )
 
 from handlers.group import GroupHandler
 
 url_patterns = [
+
+        (r"/group/([0-9]+)", GroupHandler),
         (r"/", HomeHandler),
+
         (r"/login",LoginHandler),
         (r"/logout",LogoutHandler),
         (r"/signup",SignupHandler),
-        (r"/group/([0-9]+)", GroupHandler),
-        (r"/me/feed", FeedHandler),
-        (r"/me/mylinks", MyLinksHandler),
-        (r"/me/group/([0-9]+)",MeGroupHandler),
-        (r"/me/staff_picks",StaffPicksHandler),
+
+        (r"/me/staff_picks",StaffPicksHandler),        
         (r"/me/popular_groups",PopularGroupsHandler),
         (r"/me/recent_links",RecentLinksHandler),
-        (r"/follow/user/([0-9]+)",FollowHandler)
+        
+
+        (r"/me/feed", FeedHandler),
+        (r"/me/mylinks", MyLinksHandler),
+        (r"/me/following", FollowingHandler),
+        (r"/me/follower", FollowerHandler),
+        
+        (r"/me/group/([0-9]+)",MeGroupHandler),
+
+        (r"/follow/user/([0-9]+)",FollowUserHandler),
+        (r"/follow/group/([0-9]+)",FollowGroupHandler)       
         
         ]
 
