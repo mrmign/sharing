b2ae3aeac742b845c08d87dbf447afb36f98b08d
arming <arming@Ubuntu.(none)>
2013-03-24 19:12:00 +0800

    delete unused file

--- uimodules/__init__.py
+++
@@ -1,1 +0,0 @@
-from homemodule import *
--- uimodules/homemodule.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-
-import tornado.web
-
-
-class HomeListModule(tornado.web.UIModule):
-    def render(self, group):
-        return self.render_string("modules/homelist.html", group=group)
